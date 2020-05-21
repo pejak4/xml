d74a08a8fa96da081b1301d85e28b695e669e076
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-05-21 14:49:03 +0200

    Search service model
    Add controller, service, repository and models for searching cars

---
+++ services/search-service/src/main/java/com/project/searchservice/model/Car.java
@@ -0,0 +1,45 @@
+package com.project.searchservice.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int doors;
+    private String description;
+    private String image;
+    private String brand;
+    private String model;
+    private String fuelType;
+    private String transmission;
+    private String classCar;
+    private double price;
+    private int mileage;
+    private int plannedMileage;
+    private Boolean CDW;
+    private int capacitySeats;
+    private int capacitySeatsForKids;
+    private int cubicCapacity;
+    private int horsePower;
+    private Boolean usb;
+    private Boolean gps;
+    private double fuelTankCapacity;
+    private String cityLocation;
+
+    @OneToMany(mappedBy = "car", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Set<CarRentalDate> dateList;
+
+
+}
