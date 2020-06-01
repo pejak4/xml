0cf663b4bb768ff859e77948d937203c70ce63b4
pejak4 <53259486+pejak4@users.noreply.github.com>
2020-06-01 13:21:34 +0200

    Add microservices
    With eureka server and zuul gateway

---
+++ services/rentalRequest-service/src/main/java/com/example/rentalRequestservice/RentalRequestServiceApplication.java
@@ -0,0 +1,28 @@
+package com.example.rentalRequestservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@SpringBootApplication
+public class RentalRequestServiceApplication {
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+	@Bean
+	public WebClient.Builder getWebclientBuilder() {
+		return WebClient.builder();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalRequestServiceApplication.class, args);
+	}
+
+}
---
+++ services/rentalRequest-service/src/test/java/com/example/rentalRequestservice/RentalRequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentalRequestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalRequestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
