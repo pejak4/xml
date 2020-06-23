4228ed73a27d1146d418611244606582cc4f2ae6
koske997 <koske.koske035@gmail.com>
2020-06-24 01:50:39 +0200

    Generated certificate and keystore for carservice

--- services/car-service/src/main/java/com/example/carService/CarServiceApplication.java
+++ services/car-service/src/main/java/com/example/carService/CarServiceApplication.java
@@ -1,5 +1,7 @@
 package com.example.carService;
 
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.scheduling.annotation.Scheduled;
+
+import java.security.NoSuchAlgorithmException;
 
 
 @SpringBootApplication
@@ -29,6 +33,21 @@ public class CarServiceApplication {
 		return WebClient.builder();
 	}
 
+	@Bean
+	public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
+		DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
+		System.setProperty("javax.net.ssl.keyStore", "src/main/resources/carser.jks");
+		System.setProperty("javax.net.ssl.keyStorePassword", "pass123");
+		System.setProperty("javax.net.ssl.trustStore", "src/main/resources/carser.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "pass123");
+		EurekaJerseyClientImpl.EurekaJerseyClientBuilder builder = new EurekaJerseyClientImpl.EurekaJerseyClientBuilder();
+		builder.withClientName("account-client");
+		builder.withSystemSSLConfiguration();
+		builder.withMaxTotalConnections(10);
+		builder.withMaxConnectionsPerHost(10);
+		args.setEurekaJerseyClient(builder.build());
+		return args;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarServiceApplication.class, args);
