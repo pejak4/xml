a16914336f7c2444df65a98c1fa01651aacfd0c5
koske997 <koske.koske035@gmail.com>
2020-06-24 21:10:39 +0200

    Microservices are conected to eureka through ssl connection

--- clientfrontend/src/axios-objects.js
+++ clientfrontend/src/axios-objects.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8762'
+    baseURL: 'https://localhost:8762'
 });
 
 export default instance;
--- services/authentication-service/src/main/java/com/example/demo/KtbackendApplication.java
+++ services/authentication-service/src/main/java/com/example/demo/KtbackendApplication.java
@@ -1,11 +1,15 @@
 package com.example.demo;
 
+import com.netflix.discovery.DiscoveryClient;
+import com.netflix.discovery.shared.transport.jersey.EurekaJerseyClientImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.security.NoSuchAlgorithmException;
 
 @SpringBootApplication
 public class KtbackendApplication {
@@ -19,6 +23,23 @@ public class KtbackendApplication {
 	public WebClient.Builder getWebClientBuilder() {
 		return WebClient.builder();
 	}
+
+	@Bean
+	public DiscoveryClient.DiscoveryClientOptionalArgs discoveryClientOptionalArgs() throws NoSuchAlgorithmException {
+		DiscoveryClient.DiscoveryClientOptionalArgs args = new DiscoveryClient.DiscoveryClientOptionalArgs();
+		System.setProperty("javax.net.ssl.keyStore", "src/main/resources/auth.jks");
+		System.setProperty("javax.net.ssl.keyStorePassword", "passs123");
+		System.setProperty("javax.net.ssl.trustStore", "src/main/resources/auth.jks");
+		System.setProperty("javax.net.ssl.trustStorePassword", "passs123");
+		EurekaJerseyClientImpl.EurekaJerseyClientBuilder builder = new EurekaJerseyClientImpl.EurekaJerseyClientBuilder();
+		builder.withClientName("account-client");
+		builder.withSystemSSLConfiguration();
+		builder.withMaxTotalConnections(10);
+		builder.withMaxConnectionsPerHost(10);
+		args.setEurekaJerseyClient(builder.build());
+		return args;
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(KtbackendApplication.class, args);
